3f5cd8e52234ab3b937f20b8db8821a2d4c881e4
Samuel Hellawell <sshellawell@gmail.com>
2021-04-07 19:53:51 +0100

    move getKeypairFromDerivedKey to keypairs methods

--- src/methods/keypairs.js
+++ src/methods/keypairs.js
@@ -3,6 +3,24 @@ import { X25519KeyAgreementKey2019 } from '@digitalbazaar/x25519-key-agreement-k
 import { Ed25519VerificationKey2018 } from '@digitalbazaar/ed25519-verification-key-2018';
 import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020';
 
+const keyGenerators = {
+  Ed25519VerificationKey2018: async (seed) => Ed25519VerificationKey2018.generate({
+    seed,
+  }),
+  X25519KeyAgreementKey2019: async (seed) => {
+    // X25519KeyAgreementKey2019 doesnt support seed in generate method, so we will derive
+    // from a Ed25519VerificationKey2018 keypair
+    const edPair = await Ed25519VerificationKey2018.generate({ seed });
+    return X25519KeyAgreementKey2019.fromEdKeyPair({ keyPair: edPair });
+  },
+  X25519KeyAgreementKey2020: async (seed) => {
+    // X25519KeyAgreementKey2020 doesnt support seed in generate method, so we will derive
+    // from a Ed25519VerificationKey2020 keypair
+    const edPair = await Ed25519VerificationKey2020.generate({ seed });
+    return X25519KeyAgreementKey2020.fromEd25519VerificationKey2020({ keyPair: edPair });
+  },
+};
+
 const keyConstructors = {
   Ed25519VerificationKey2018: (keypairOptions) => new Ed25519VerificationKey2018(keypairOptions),
   Ed25519VerificationKey2020: (keypairOptions) => new Ed25519VerificationKey2020(keypairOptions),
@@ -46,4 +64,21 @@ export function getKeydocFromPair(keyPair) {
     return keyPair.export({ publicKey: true, privateKey: true });
   }
   return keyPair;
+}
+
+export async function getKeypairFromDerivedKey(derivedKey, type = 'X25519KeyAgreementKey2020') {
+  const keyGenerator = keyGenerators[type];
+  if (!keyGenerator) {
+    throw new Error(`Unable to generate keypair for type: ${type}`);
+  }
+
+  // Generate keypair
+  const kp = await keyGenerator(derivedKey);
+
+  // Assign controller and ID as fingerprint
+  const fingerprint = kp.fingerprint();
+  kp.controller = `did:key:${fingerprint}`;
+  kp.id = `${kp.controller}#${fingerprint}`;
+
+  return kp;
 }
--- src/methods/password.js
+++ src/methods/password.js
@@ -1,26 +1,5 @@
-import { X25519KeyAgreementKey2020 } from '@digitalbazaar/x25519-key-agreement-key-2020';
-import { X25519KeyAgreementKey2019 } from '@digitalbazaar/x25519-key-agreement-key-2019';
-import { Ed25519VerificationKey2018 } from '@digitalbazaar/ed25519-verification-key-2018';
-import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020';
+import { getKeypairFromDerivedKey } from './keypairs';
 import crypto from '../crypto';
-
-const keyGenerators = {
-  Ed25519VerificationKey2018: async (seed) => Ed25519VerificationKey2018.generate({
-    seed,
-  }),
-  X25519KeyAgreementKey2019: async (seed) => {
-    // X25519KeyAgreementKey2019 doesnt support seed in generate method, so we will derive
-    // from a Ed25519VerificationKey2018 keypair
-    const edPair = await Ed25519VerificationKey2018.generate({ seed });
-    return X25519KeyAgreementKey2019.fromEdKeyPair({ keyPair: edPair });
-  },
-  X25519KeyAgreementKey2020: async (seed) => {
-    // X25519KeyAgreementKey2020 doesnt support seed in generate method, so we will derive
-    // from a Ed25519VerificationKey2020 keypair
-    const edPair = await Ed25519VerificationKey2020.generate({ seed });
-    return X25519KeyAgreementKey2020.fromEd25519VerificationKey2020({ keyPair: edPair });
-  },
-};
 
 export async function passwordToKey(
   password,
@@ -55,23 +34,6 @@ export async function passwordToKey(
     ))
     .then((webKey) => crypto.subtle.exportKey('raw', webKey))
     .then((buffer) => new Uint8Array(buffer));
-}
-
-export async function getKeypairFromDerivedKey(derivedKey, type = 'X25519KeyAgreementKey2020') {
-  const keyGenerator = keyGenerators[type];
-  if (!keyGenerator) {
-    throw new Error(`Unable to generate keypair for type: ${type}`);
-  }
-
-  // Generate keypair
-  const kp = await keyGenerator(derivedKey);
-
-  // Assign controller and ID as fingerprint
-  const fingerprint = kp.fingerprint();
-  kp.controller = `did:key:${fingerprint}`;
-  kp.id = `${kp.controller}#${fingerprint}`;
-
-  return kp;
 }
 
 export async function passwordToKeypair(password) {
