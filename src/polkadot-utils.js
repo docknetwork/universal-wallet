431994f02ea3708502d93d787cfc41e053a9f19c
Samuel Hellawell <sshellawell@gmail.com>
2021-05-18 01:24:12 +0100

    change polkadot key id generation to encode public key

--- src/polkadot-utils.js
+++ src/polkadot-utils.js
@@ -29,36 +29,36 @@ const TYPE_FROM_SEED = {
 
 // TODO: maybe make this an SDK method instead?
 export function polkadotToKeydoc(polkadotKeys, controller = undefined, keyPassphrase = 'test') {
-    const keyjson = polkadotKeys.toJson(keyPassphrase); // TODO: update method to import from json out of band
-    const polkadotType = polkadotKeys.type || getKeyPairType(polkadotKeys);
+  const keyjson = polkadotKeys.toJson(keyPassphrase); // TODO: update method to import from json out of band
+  const polkadotType = polkadotKeys.type || getKeyPairType(polkadotKeys);
 
-    // NOTE: polkadotKeys.publicKey and publicKey from decodePair result are different for ecdsa type by an extra value on the end
-    const decoded = decodePair(keyPassphrase, base64Decode(keyjson.encoded), keyjson.encoding.type);
+  // NOTE: polkadotKeys.publicKey and publicKey from decodePair result are different for ecdsa type by an extra value on the end
+  const decoded = decodePair(keyPassphrase, base64Decode(keyjson.encoded), keyjson.encoding.type);
 
-    let publicKey, secretKey;
-    if (decoded.secretKey.length === 64) {
-      publicKey = decoded.publicKey;
-      secretKey = decoded.secretKey;
-    } else {
-      const pair = TYPE_FROM_SEED[polkadotType](decoded.secretKey);
-      publicKey = pair.publicKey;
-      secretKey = pair.secretKey;
-    }
+  let publicKey, secretKey;
+  if (decoded.secretKey.length === 64) {
+    publicKey = decoded.publicKey;
+    secretKey = decoded.secretKey;
+  } else {
+    const pair = TYPE_FROM_SEED[polkadotType](decoded.secretKey);
+    publicKey = pair.publicKey;
+    secretKey = pair.secretKey;
+  }
 
-    const kpType = polkadotTypesToKeys[polkadotType];
-    if (!kpType) {
-      throw new Error(`Unknown polkadot type: ${polkadotType}`);
-    }
+  const kpType = polkadotTypesToKeys[polkadotType];
+  if (!kpType) {
+    throw new Error(`Unknown polkadot type: ${polkadotType}`);
+  }
 
-    const formattedkeyDoc = {
-      id: `${controller}#keys-1`,
-      controller,
-      type: kpType,
-      publicKeyBase58: bs58.encode(publicKey),
-      privateKeyBase58: bs58.encode(secretKey),
-      publicKeyMultibase: `z${bs58.encode(publicKey)}`,
-      privateKeyMultibase: `z${bs58.encode(secretKey)}`,
-    };
+  const formattedkeyDoc = {
+    id: `${controller}#${encodeAddress(publicKey)}`,
+    controller,
+    type: kpType,
+    publicKeyBase58: bs58.encode(publicKey),
+    privateKeyBase58: bs58.encode(secretKey),
+    publicKeyMultibase: `z${bs58.encode(publicKey)}`,
+    privateKeyMultibase: `z${bs58.encode(secretKey)}`,
+  };
 
   // auto create controller
   if (!controller) {
@@ -66,7 +66,7 @@ export function polkadotToKeydoc(polkadotKeys, controller = undefined, keyPassph
     const fingerprint = keypairInstance.fingerprint();
     if (!formattedkeyDoc.controller) {
       formattedkeyDoc.controller = `did:key:${fingerprint}`;
-      formattedkeyDoc.id = `did:key:${fingerprint}#${fingerprint}`;
+      formattedkeyDoc.id = `${formattedkeyDoc.controller}#${fingerprint}`;
     }
   }
   return formattedkeyDoc;
