84852d4d68c18efee2d358d40c9d3fb399dbaa1e
Samuel Hellawell <sshellawell@gmail.com>
2022-07-06 17:14:17 +0100

    Add code coverage
    Signed-off-by: Samuel Hellawell <sshellawell@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   clearMocks: true,
   testTimeout: 30000,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    }
+  },
   testEnvironment: "./tests/test-environment.js",
   transform: {
     "^.+\\.(ts|js)$": "babel-jest"
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!@polkadot|@babel)"
+    "/node_modules/(?!@polkadot|@babel|@docknetwork)"
   ],
   setupFilesAfterEnv: [
     "./tests/setup-test-env.js"
